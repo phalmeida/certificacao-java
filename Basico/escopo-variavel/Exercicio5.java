3105b5e0c85451ff3a28fbeb46c414d6ba3c027a
Philipe Allan Almeida <philipe.almeida@engesoftware.com.br>
2020-03-02 22:17:05 -0300

    Exercício número 5

---
+++ Basico/escopo-variavel/Exercicio5.java
@@ -0,0 +1,9 @@
+class Exercicio5 {
+	static int i = 3;
+	public static void main(String[] a) {
+		for(new Exercicio5().i = 10; new Exercicio5().i < 100; new Exercicio5().i++){
+			System.out.println(i);
+		}
+
+	}
+}
