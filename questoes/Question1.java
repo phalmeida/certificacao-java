500b7fa49cf61bb021531be68caab4fe9fa19d34
Philipe Allan Almeida <philipe.almeida@outlook.com>
2020-03-03 09:51:48 -0300

    Questão 1

---
+++ questoes/Question1.java
@@ -0,0 +1,35 @@
+class Question1 {
+    public static void main(String[] args) {
+        WildAnimal wolf = new WildAnimal("long");
+        WildAnimal tiger = new WildAnimal("Feline", 80, "Short");
+        System.out.println(wolf.type + " " + wolf.maxSpeed + " " + wolf.bounds);
+        System.out.println(tiger.type + " " + tiger.maxSpeed + " " + tiger.bounds);
+    }
+}
+
+class WildAnimal extends Animal {
+    String bounds;
+
+    WildAnimal(String bounds) {
+        super();
+        this.bounds = bounds;
+    }
+
+    WildAnimal(String type, int maxSpeed, String bounds) {
+        super(type, maxSpeed);
+        this.bounds = bounds;
+    }
+}
+
+class Animal {
+    String type = "Canine";
+    int maxSpeed = 60;
+
+    Animal() {
+    }
+
+    Animal(String type, int maxSpeed) {
+        this.type = type;
+        this.maxSpeed = maxSpeed;
+    }
+}
