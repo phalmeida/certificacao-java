8d26e930aa2603baa84f562648d672aa7316d38d
Philipe Allan Almeida <philipe.almeida@outlook.com>
2020-03-11 13:49:38 -0300

    Questão 12

---
+++ questoes/Question12.java
@@ -0,0 +1,10 @@
+class Question12 {
+    public static void main(String[] args) {
+        int n[][] = {{1, 3}, {2, 4}};
+        for (int i = n.length - 1; i >= 0; i--) {
+            for (int y : n[i]) {
+                System.out.print(y);
+            }
+        }
+    }
+}
