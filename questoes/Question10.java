b6b35d2eebdc65a99fc9c84749cb925fcf6eedb8
Philipe Allan Almeida <philipe.almeida@outlook.com>
2020-03-11 13:34:03 -0300

    Questão 10

---
+++ questoes/Question10.java
@@ -0,0 +1,12 @@
+class Question10 {
+    public static void main(String[] args) {
+        String str1 = "Java";
+        String str2 = new String("java");
+
+        if (str2.equals(str1.toLowerCase())){
+            System.out.println("Equal");
+        } else{
+            System.out.println("Not Equal");
+        }
+    }
+}
