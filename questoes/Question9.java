370624249f5d06ebad96004150992aa2fdc91c7e
Philipe Allan Almeida <philipe.almeida@basis.com.br>
2020-03-11 13:22:08 -0300

    Questão 9

---
+++ questoes/Question9.java
@@ -0,0 +1,9 @@
+class Question9 {
+    public static void main(String[] args) {
+        String arg1 = args[0];
+        String arg2 = args[1];
+        String arg3 = args[2];
+
+        System.out.println("Arg is " + arg3);
+    }
+}
